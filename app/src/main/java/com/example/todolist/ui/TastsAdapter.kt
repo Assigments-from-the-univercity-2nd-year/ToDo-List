7d70a52e514f00aa2b42b4f5605c4573b1da5e00
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-10-23 14:57:36 +0300

    Add: TasksAdapter.kt

---
+++ app/src/main/java/com/example/todolist/ui/TastsAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.todolist.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.data.Task
+import com.example.todolist.databinding.ItemTaskBinding
+
+class TastsAdapter : ListAdapter<Task, TastsAdapter.TasksViewHolder>(DiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val binding: ItemTaskBinding =
+            ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TasksViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        val currentTask = getItem(position)
+        holder.bind(currentTask)
+    }
+
+    class TasksViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.task = task
+        }
+    }
+
+    class DiffCallBack : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
