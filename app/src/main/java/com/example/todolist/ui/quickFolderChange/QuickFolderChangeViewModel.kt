cebcf2ef929c59e10bc0f3057d0ac5e1e4d07b23
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-11-15 19:20:03 +0200

    Refactored:custom layout when choosing quick folder change

--- app/src/main/java/com/example/todolist/ui/quickFolderChange/QuickFolderChangeViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/quickFolderChange/QuickFolderChangeViewModel.kt
@@ -1,21 +1,21 @@
 package com.example.todolist.ui.quickFolderChange
 
-import android.R.layout.select_dialog_item
 import android.content.Context
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModel
+import com.example.todolist.R
 import com.example.todolist.data.Folder
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
 
 class QuickFolderChangeViewModel : ViewModel() {
 
     private lateinit var pinnedFolders: Array<Folder>
 
     fun getStringOfPinnedFolders(context: Context, arguments: Bundle?): ArrayAdapter<String> {
-        val adapter = ArrayAdapter<String>(context, select_dialog_item)
-        pinnedFolders = QuickFolderChangeDialogFragmentArgs.fromBundle(arguments ?: Bundle.EMPTY).pinnedFolders
+        val adapter = ArrayAdapter<String>(context, R.layout.item_folder_pinned)
+        pinnedFolders = QuickFolderChangeDialogFragmentArgs
+            .fromBundle(arguments ?: Bundle.EMPTY)
+            .pinnedFolders
         val list = List(pinnedFolders.size) {
             pinnedFolders[it].title + " (${pinnedFolders[it].numberOfSubComponents})"
         }
