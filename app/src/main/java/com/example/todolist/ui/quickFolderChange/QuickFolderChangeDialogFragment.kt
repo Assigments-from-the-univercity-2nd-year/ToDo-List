96b6497a9998843383ee71e9b6dff35058d49fcc
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-11-18 11:47:07 +0200

    Refactored: quick switching between folders - better code design

--- app/src/main/java/com/example/todolist/ui/quickFolderChange/QuickFolderChangeDialogFragment.kt
+++ app/src/main/java/com/example/todolist/ui/quickFolderChange/QuickFolderChangeDialogFragment.kt
@@ -2,27 +2,35 @@ package com.example.todolist.ui.quickFolderChange
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.todolist.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class QuickFolderChangeDialogFragment : DialogFragment() {
 
-    private val viewModel: QuickFolderChangeViewModel by viewModels()
+    private val args: QuickFolderChangeDialogFragmentArgs by navArgs()
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val adapter = ArrayAdapter<String>(requireContext(), R.layout.item_folder_pinned)
+        val list = List(args.pinnedFolders.size) {
+            args.pinnedFolders[it].title + " (${args.pinnedFolders[it].numberOfSubComponents})"
+        }
+        adapter.addAll(list)
+
+        return AlertDialog.Builder(requireContext())
             .setTitle("Select the folder")
-            .setAdapter(viewModel.getStringOfPinnedFolders(requireContext(), arguments)) { _, which ->
+            .setAdapter(adapter) { _, which ->
                 setFragmentResult(
                     "folder_to_change_request",
-                    bundleOf("folder_to_change_result" to viewModel.getListElement(which))
+                    bundleOf("folder_to_change_result" to args.pinnedFolders[which])
                 )
             }.create()
+    }
 }
